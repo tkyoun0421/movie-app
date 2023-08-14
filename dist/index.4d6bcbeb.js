eaebd61f37f17c344d537868541f97172839cf8a
Taegwan Youn <tkyoun0421@naver.com>
2023-08-14 16:47:27 +0900

    next

--- dist/index.4d6bcbeb.js
+++ dist/index.4d6bcbeb.js
@@ -642,7 +642,7 @@ class Store {
             get: ()=>state[key],
             set: (val)=>{
                 state[key] = val;
-                this.observers[key].forEach((observer)=>observer(val));
+                if (Array.isArray(this.observers[key])) this.observers[key].forEach((observer)=>observer(val));
             }
         });
     }
@@ -740,6 +740,8 @@ exports.default = Headline;
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 var _heropy = require("../core/heropy");
+var _movie = require("../store/movie");
+var _movieDefault = parcelHelpers.interopDefault(_movie);
 class Search extends (0, _heropy.Component) {
     render() {
         this.el.classList.add("search");
@@ -750,15 +752,36 @@ class Search extends (0, _heropy.Component) {
             </button>
         `;
         const inputEl = this.el.querySelector("input");
-        inputEl.addEventListener("input", ()=>{});
+        inputEl.addEventListener("input", ()=>{
+            (0, _movieDefault.default).state.searchText = inputEl.value;
+        });
         inputEl.addEventListener("keydown", (event)=>{
-            event.key;
+            if (event.key === "Enter" && (0, _movieDefault.default).state.searchText.trim()) (0, _movie.searchMovies)(1);
         });
         const btnEl = this.el.querySelector(".btn");
-        btnEl.addEventListener("click", ()=>{});
+        btnEl.addEventListener("click", ()=>{
+            if ((0, _movieDefault.default).state.searchText.trim()) (0, _movie.searchMovies)(1);
+        });
     }
 }
 exports.default = Search;
+
+},{"../core/heropy":"57bZf","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","../store/movie":"kq1bo"}],"kq1bo":[function(require,module,exports) {
+var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
+parcelHelpers.defineInteropFlag(exports);
+parcelHelpers.export(exports, "searchMovies", ()=>searchMovies);
+var _heropy = require("../core/heropy");
+const store = new (0, _heropy.Store)({
+    searchText: "",
+    page: 1,
+    movies: []
+});
+exports.default = store;
+const searchMovies = async (page)=>{
+    const res = await fetch(`https://www.omdbapi.com/?apikey=7035c60c&s=${store.state.searchText}&page=${page}`);
+    const json = await res.json();
+    console.log(json);
+};
 
 },{"../core/heropy":"57bZf","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}]},["f3BSW","gLLPy"], "gLLPy", "parcelRequire6588")
 
--- src/core/heropy.js
+++ src/core/heropy.js
@@ -53,7 +53,11 @@ export class Store {
                 get: () => state[key],
                 set: (val) => {
                     state[key] = val;
-                    this.observers[key].forEach((observer) => observer(val));
+                    if (Array.isArray(this.observers[key])) {
+                        this.observers[key].forEach((observer) =>
+                            observer(val)
+                        );
+                    }
                 },
             });
         }
