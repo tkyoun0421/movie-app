246f3ab3eb11e424321d3ca6fae3a70c0a7e9a42
Taegwan Youn <tkyoun0421@naver.com>
2023-08-14 18:06:48 +0900

    next

--- dist/index.4d6bcbeb.js
+++ dist/index.4d6bcbeb.js
@@ -782,21 +782,35 @@ const store = new (0, _heropy.Store)({
     page: 1,
     pageMax: 1,
     movies: [],
-    loading: false
+    loading: false,
+    message: "Search for the movie title!"
 });
 exports.default = store;
 const searchMovies = async (page)=>{
     store.state.loading = true;
     store.state.page = page;
-    if (page === 1) store.state.movies = [];
-    const res = await fetch(`https://www.omdbapi.com/?apikey=7035c60c&s=${store.state.searchText}&page=${page}`);
-    const { Search, totalResults } = await res.json();
-    store.state.movies = [
-        ...store.state.movies,
-        ...Search
-    ];
-    store.state.pageMax = Math.ceil(Number(totalResults) / 10);
-    store.state.loading = false;
+    if (page === 1) {
+        store.state.movies = [];
+        store.state.message = "";
+    }
+    try {
+        const res = await fetch(`https://www.omdbapi.com/?apikey=7035c60c&s=${store.state.searchText}&page=${page}`);
+        const { Search, totalResults, Response, Error } = await res.json();
+        if (Response === "True") {
+            store.state.movies = [
+                ...store.state.movies,
+                ...Search
+            ];
+            store.state.pageMax = Math.ceil(Number(totalResults) / 10);
+        } else {
+            store.state.message = Error;
+            store.state.pageMax = 1;
+        }
+    } catch (error) {
+        console.log("searchMovies error:", error);
+    } finally{
+        store.state.loading = false;
+    }
 };
 
 },{"../core/heropy":"57bZf","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"8UDl3":[function(require,module,exports) {
@@ -816,15 +830,18 @@ class MovieList extends (0, _heropy.Component) {
         (0, _movieDefault.default).subscribe("loading", ()=>{
             this.render();
         });
+        (0, _movieDefault.default).subscribe("message", ()=>{
+            this.render();
+        });
     }
     render() {
         this.el.classList.add("movie-list");
         this.el.innerHTML = /* html */ `
-            <div class="movies"></div>
+            ${(0, _movieDefault.default).state.message ? `<div class="message">${(0, _movieDefault.default).state.message}</div>` : `<div class="movies"></div>`}
             <div class="the-loader hide"></div>
         `;
         const moviesEl = this.el.querySelector(".movies");
-        moviesEl.append(...(0, _movieDefault.default).state.movies.map((movie)=>new (0, _movieitemDefault.default)({
+        moviesEl?.append(...(0, _movieDefault.default).state.movies.map((movie)=>new (0, _movieitemDefault.default)({
                 movie
             }).el));
         const loaderEl = this.el.querySelector(".the-loader");
@@ -883,6 +900,7 @@ class MovieListMore extends (0, _heropy.Component) {
         this.el.classList.add("btn", "view-more", "hide");
         this.el.textContent = "View More..";
         this.el.addEventListener("click", async ()=>{
+            this.el.classList.add("hide");
             await (0, _movie.searchMovies)((0, _movieDefault.default).state.page + 1);
         });
     }
--- src/store/movie.js
+++ src/store/movie.js
@@ -6,6 +6,7 @@ const store = new Store({
     pageMax: 1,
     movies: [],
     loading: false,
+    message: 'Search for the movie title!',
 });
 
 export default store;
@@ -15,12 +16,23 @@ export const searchMovies = async (page) => {
     store.state.page = page;
     if (page === 1) {
         store.state.movies = [];
+        store.state.message = '';
     }
-    const res = await fetch(
-        `https://www.omdbapi.com/?apikey=7035c60c&s=${store.state.searchText}&page=${page}`
-    );
-    const { Search, totalResults } = await res.json();
-    store.state.movies = [...store.state.movies, ...Search];
-    store.state.pageMax = Math.ceil(Number(totalResults) / 10);
-    store.state.loading = false;
+    try {
+        const res = await fetch(
+            `https://www.omdbapi.com/?apikey=7035c60c&s=${store.state.searchText}&page=${page}`
+        );
+        const { Search, totalResults, Response, Error } = await res.json();
+        if (Response === 'True') {
+            store.state.movies = [...store.state.movies, ...Search];
+            store.state.pageMax = Math.ceil(Number(totalResults) / 10);
+        } else {
+            store.state.message = Error;
+            store.state.pageMax = 1;
+        }
+    } catch (error) {
+        console.log('searchMovies error:', error);
+    } finally {
+        store.state.loading = false;
+    }
 };
