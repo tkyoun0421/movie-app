e9d1d7aa3d4680520e29213b6df0f35e4cf108d6
Taegwan Youn <tkyoun0421@naver.com>
2023-08-14 17:20:15 +0900

    next

--- dist/index.4d6bcbeb.js
+++ dist/index.4d6bcbeb.js
@@ -799,6 +799,8 @@ parcelHelpers.defineInteropFlag(exports);
 var _heropy = require("../core/heropy");
 var _movie = require("../store/movie");
 var _movieDefault = parcelHelpers.interopDefault(_movie);
+var _movieitem = require("./Movieitem");
+var _movieitemDefault = parcelHelpers.interopDefault(_movieitem);
 class MovieList extends (0, _heropy.Component) {
     constructor(){
         super();
@@ -812,13 +814,43 @@ class MovieList extends (0, _heropy.Component) {
             <div class="movies"></div>
         `;
         const moviesEl = this.el.querySelector(".movies");
-        moviesEl.append((0, _movieDefault.default).state.movies.map((movie)=>{
-            return movie.Title;
-        }));
+        moviesEl.append(...(0, _movieDefault.default).state.movies.map((movie)=>new (0, _movieitemDefault.default)({
+                movie
+            }).el));
     }
 }
 exports.default = MovieList;
 
-},{"../core/heropy":"57bZf","../store/movie":"kq1bo","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}]},["f3BSW","gLLPy"], "gLLPy", "parcelRequire6588")
+},{"../core/heropy":"57bZf","../store/movie":"kq1bo","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","./Movieitem":"534Hw"}],"534Hw":[function(require,module,exports) {
+var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
+parcelHelpers.defineInteropFlag(exports);
+var _heropy = require("../core/heropy");
+class MovieItem extends (0, _heropy.Component) {
+    constructor(props){
+        super({
+            props,
+            tagName: "a"
+        });
+    }
+    render() {
+        const { movie } = this.props;
+        this.el.setAttribute("href", `#/movie?id=${movie.imdbID}`);
+        this.el.classList.add("movie");
+        this.el.style.backgroundImage = `url(${movie.Poster})`;
+        this.el.innerHTML = /* html */ `
+            <div class="info">
+                <div class="year">
+                    ${movie.Year}
+                </div>
+                <div class="title">
+                    ${movie.Title}
+                </div>
+            </div>
+        `;
+    }
+}
+exports.default = MovieItem;
+
+},{"../core/heropy":"57bZf","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}]},["f3BSW","gLLPy"], "gLLPy", "parcelRequire6588")
 
 //# sourceMappingURL=index.4d6bcbeb.js.map
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,5 +1,6 @@
 import { Component } from '../core/heropy';
 import movieStore from '../store/movie';
+import MovieItem from './Movieitem';
 
 export default class MovieList extends Component {
     constructor() {
@@ -16,9 +17,12 @@ export default class MovieList extends Component {
 
         const moviesEl = this.el.querySelector('.movies');
         moviesEl.append(
-            movieStore.state.movies.map((movie) => {
-                return movie.Title;
-            })
+            ...movieStore.state.movies.map(
+                (movie) =>
+                    new MovieItem({
+                        movie,
+                    }).el
+            )
         );
     }
 }
